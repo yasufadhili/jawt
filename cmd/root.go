eec47f604e4eedf7c1a670808861e1378c24bbad
Yasu Fadhili <yasufad@proton.me>
2025-06-21 14:25:35 +0100

    (#18): implement root command

---
+++ cmd/root.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "jawt",
+	Short: "JAWT - Just Another Web Tool",
+	Long: `JAWT is a tool for creating, developing, and building minimal web applications.
+It offers a streamlined workflow and unified development experience.`,
+}
+
+// Execute executes the root command.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolP("help", "h", false, "Display help information for the command")
+	rootCmd.Flags().BoolP("version", "v", false, "Display JAWT version information")
+
+	rootCmd.AddCommand(initCmd)
+	rootCmd.AddCommand(runCmd)
+	rootCmd.AddCommand(buildCmd)
+	rootCmd.AddCommand(versionCmd)
+}
