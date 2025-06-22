ac1fbd42a6ca06e93f41d4972e3cf1018ca83d2c
Yasu Fadhili <yasufad@proton.me>
2025-06-22 10:08:36 +0100

    add format errors helper (#22)

--- internal/page_compiler/page_compiler.go
+++ internal/page_compiler/page_compiler.go
@@ -21,6 +21,27 @@ func (pc *PageCompiler) CompilePage() error {
 
 	fmt.Printf("Compiling page %s:%s\n", pc.pageInfo.Name, pc.pageInfo.AbsolutePath)
 
+	input, err := antlr.NewFileStream(pc.pageInfo.AbsolutePath)
+	if err != nil {
+		return err
+	}
+	lexer := parser.NewJMLPageLexer(input)
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+
+	p := parser.NewJMLPageParser(stream)
+	p.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
+
+	tree := p.Page()
+
+	fmt.Println(tree.ToStringTree(nil, p))
+
+	// Build AST
+	astBuilder := NewAstBuilder()
+	astRoot := astBuilder.Visit(tree).(*Page)
+	if astRoot.Doctype != nil {
+		fmt.Printf("AST Built: %+v\n", astRoot.Doctype) // Just a quick check
+	}
+
 	pc.pageInfo.Compiled = true
 	return nil
 }
@@ -52,4 +73,16 @@ func (pc *PageCompiler) parsePage() (parser.IPageContext, []error) {
 	p.AddErrorListener(antlr.NewDiagnosticErrorListener(true))
 
 	return tree, nil
+}
+
+// Helper function to format a slice of errors into a single string
+func formatErrors(errors []error) string {
+	if len(errors) == 0 {
+		return "No errors."
+	}
+	s := ""
+	for i, err := range errors {
+		s += fmt.Sprintf("  %d. %s\n", i+1, err.Error())
+	}
+	return s
 }
