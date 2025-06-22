820fa915747be6d0e222f032edc32469ce7ef4d1
Yasu Fadhili <yasufad@proton.me>
2025-06-22 07:46:59 +0100

    use placeholder ast builder (#22)

--- internal/page_compiler/ast_builder.go
+++ internal/page_compiler/ast_builder.go
@@ -3,6 +3,8 @@ package page_compiler
 import (
 	"github.com/antlr4-go/antlr/v4"
 	parser "github.com/yasufadhili/jawt/internal/pc/parser/generated"
+	"strconv"
+	"strings"
 )
 
 type AstBuilder struct {
@@ -35,4 +37,66 @@ func (ab *AstBuilder) Visit(tree antlr.ParseTree) interface{} {
 	}
 
 	return nil // For any unhandled types
+}
+
+func (ab *AstBuilder) VisitPage(ctx *parser.PageContext) interface{} {
+	p := &Page{}
+	return p
+}
+
+func (ab *AstBuilder) VisitDoctypeSpecifier(ctx *parser.DoctypeSpecifierContext) interface{} {
+	return &DoctypeSpecifier{
+		Doctype: ctx.Doctype().GetText(),
+		Name:    ctx.IDENTIFIER().GetText(),
+	}
+}
+
+func (ab *AstBuilder) VisitImportStatement(ctx *parser.ImportStatementContext) interface{} {
+	// Remove quotes from the string literal
+	fromPath := strings.Trim(ctx.STRING().GetText(), `"`)
+
+	return &ImportStatement{
+		Doctype:    ctx.Doctype().GetText(),
+		Identifier: ctx.IDENTIFIER().GetText(),
+		From:       fromPath,
+	}
+}
+
+func (ab *AstBuilder) VisitPageDefinition(ctx *parser.PageDefinitionContext) interface{} {
+	pd := &PageDefinition{}
+	return pd
+}
+
+func (ab *AstBuilder) VisitPageProperty(ctx *parser.PagePropertyContext) interface{} {
+	val := ctx.PropertyValue().Accept(ab)
+	return &PageProperty{
+		Key:   ctx.IDENTIFIER().GetText(),
+		Value: val,
+	}
+}
+
+func (ab *AstBuilder) VisitLiteral(ctx *parser.LiteralContext) interface{} {
+	if ctx.INTEGER() != nil {
+		val, err := strconv.Atoi(ctx.INTEGER().GetText())
+		if err != nil {
+			// Handle parsing error appropriately (log, return error, etc.)
+			return 0
+		}
+		return val
+	} else if ctx.STRING() != nil {
+		// Remove quotes from the string literal
+		str := ctx.STRING().GetText()
+		if len(str) >= 2 && str[0] == '"' && str[len(str)-1] == '"' {
+			str = str[1 : len(str)-1]
+		}
+		return str
+	} else if ctx.IDENTIFIER() != nil {
+		return ctx.IDENTIFIER().GetText() // Treat as raw identifier for now
+	}
+	return nil
+}
+
+// VisitPropertyValue just delegates to VisitLiteral
+func (ab *AstBuilder) VisitPropertyValue(ctx *parser.PropertyValueContext) interface{} {
+	return ab.Visit(ctx.Literal())
 }
