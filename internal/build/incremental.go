07b3657e4433745f985af061bfc79500b75bb961
Yasu Fadhili <yasufad@proton.me>
2025-06-30 15:31:46 +0100

    restrcture incremental

--- internal/build/incremental.go
+++ internal/build/incremental.go
@@ -1,15 +1,16 @@
 package build
 
+import "fmt"
+
 // BuildIncremental performs incremental build with error handling
 func (b *Builder) BuildIncremental() error {
 	b.mu.Lock()
 	defer b.mu.Unlock()
 
-	//if b.compiler == nil {
-	//	return fmt.Errorf("compiler not initialised - run full build first")
-	//}
+	if b.compiler == nil {
+		return fmt.Errorf("compiler not initialised - run full build first")
+	}
 
-	/**
 	if err := b.compiler.CompileChanged(); err != nil {
 		buildErr := fmt.Errorf("incremental compilation failed: %w", err)
 		if b.errorState.shouldShowError(buildErr) {
@@ -17,8 +18,6 @@ func (b *Builder) BuildIncremental() error {
 		}
 		return buildErr
 	}
-
-	*/
 
 	if b.errorState.shouldShowError(nil) {
 		b.printIncrementalSuccess()
