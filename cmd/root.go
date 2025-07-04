d08bbdec2def120b6de6820b957a316f79ad3e1e
Yasu Fadhili <yasufad@proton.me>
2025-07-04 10:39:53 +0100

    refactor: use proper Jawt acronym

--- cmd/root.go
+++ cmd/root.go
@@ -10,7 +10,7 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "jawt",
 	Short: "JAWT - Just Another Web Tool",
-	Long: `JAWT is a tool for creating, developing, and building minimal web applications.
+	Long: `JAWT is a tool for creating, developing, and building web applications.
 It offers a streamlined workflow and unified development experience.`,
 	PersistentPreRun: func(cmd *cobra.Command, args []string) {
 	},
@@ -26,7 +26,7 @@ func Execute() {
 
 func init() {
 	rootCmd.PersistentFlags().BoolP("help", "h", false, "Display help information for the command")
-	rootCmd.Flags().BoolP("version", "v", false, "Display JAWT version information")
+	rootCmd.Flags().BoolP("version", "v", false, "Display Jawt version information")
 
 	// rootCmd.AddCommand(initCmd)
 	// rootCmd.AddCommand(runCmd)
