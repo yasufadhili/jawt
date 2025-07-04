88f8cef7cc02e0efc9a5497f43a05e2fc00bf160
Yasu Fadhili <yasufad@proton.me>
2025-07-04 11:11:23 +0100

    refactor: remove previous implementation

--- internal/common/common.go
+++
@@ -1,9 +0,0 @@
-package common
-
-// BuildTarget represents different output formats
-type BuildTarget int
-
-const (
-	TargetPage BuildTarget = iota
-	TargetComponent
-)
--- internal/tsc/sourcemap/source.go
+++
@@ -1,9 +0,0 @@
-package sourcemap
-
-import "github.com/yasufadhili/jawt/internal/tsc/core"
-
-type Source interface {
-	Text() string
-	FileName() string
-	LineMap() []core.TextPos
-}
