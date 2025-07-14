91f80be9ce8e880ea955a4888433751f80a0676d
Yasu Fadhili <yasufad@proton.me>
2025-07-14 18:34:45 +0100

    feat(compiler): implement basic JML parsing and AST building

--- internal/compiler/ast_builder.go
+++ internal/compiler/ast_builder.go
@@ -1,1 +1,39 @@
 package compiler
+
+import (
+	"github.com/yasufadhili/jawt/internal/ast"
+	"github.com/yasufadhili/jawt/internal/compiler/parser"
+	"github.com/yasufadhili/jawt/internal/diagnostic"
+)
+
+type AstBuilder struct {
+	*parser.BaseJmlVisitor
+	reporter *diagnostic.Reporter
+	file     string
+}
+
+func NewAstBuilder(file string, reporter *diagnostic.Reporter) *AstBuilder {
+	return &AstBuilder{
+		BaseJmlVisitor: &parser.BaseJmlVisitor{},
+		reporter:       reporter,
+		file:           file,
+	}
+}
+
+func (b *AstBuilder) VisitDocument(ctx *parser.DocumentContext) interface{} {
+	docTypeStr := ctx.DOCTYPE().GetText()
+	docType := ast.DocumentTypePage
+	if docTypeStr == "_doctype component" {
+		docType = ast.DocumentTypeComponent
+	}
+
+	name := ctx.IDENTIFIER().GetText()
+
+	// For now, just return a basic document. More complex AST building will go here.
+	return &ast.Document{
+		Position:   ast.Position{Line: ctx.GetStart().GetLine(), Column: ctx.GetStart().GetColumn(), File: b.file},
+		DocType:    docType,
+		Name:       ast.NewIdentifier(ast.Position{Line: ctx.IDENTIFIER().GetSymbol().GetLine(), Column: ctx.IDENTIFIER().GetSymbol().GetColumn(), File: b.file}, name),
+		SourceFile: b.file,
+	}
+}
--- internal/compiler/compiler.go
+++ internal/compiler/compiler.go
@@ -1,7 +1,12 @@
 package compiler
 
 import (
+	"fmt"
+	"os"
+
+	"github.com/antlr4-go/antlr/v4"
 	"github.com/yasufadhili/jawt/internal/ast"
+	"github.com/yasufadhili/jawt/internal/compiler/parser"
 	"github.com/yasufadhili/jawt/internal/core"
 	"github.com/yasufadhili/jawt/internal/diagnostic"
 )
@@ -18,5 +23,25 @@ func NewCompiler(ctx *core.JawtContext) *Compiler {
 
 // Compile compiles a single JML file.
 func (c *Compiler) Compile(file string, reporter *diagnostic.Reporter) (*ast.Document, error) {
-	return nil, nil
+	input, err := antlr.NewFileStream(file)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read JML file %s: %w", file, err)
+	}
+
+	lexer := parser.NewJmlLexer(input)
+	stream := antlr.NewCommonTokenStream(lexer, antlr.TokenDefaultChannel)
+	parser := parser.NewJmlParser(stream)
+
+	// Remove default error listeners and add our custom one
+	parser.RemoveErrorListeners()
+	parser.AddErrorListener(diagnostic.NewAntlrErrorListener(file, reporter))
+
+	// Parse the input
+	tree := parser.Document()
+
+	// Build the AST
+	builder := NewAstBuilder(file, reporter)
+	astDoc := builder.Visit(tree).(*ast.Document)
+
+	return astDoc, nil
 }
