431036cfc40cd736d116303732ed73c3eef20ebd
Yasu Fadhili <yasufad@proton.me>
2025-07-14 15:06:16 +0100

    implement compiler placeholder

--- internal/compiler/compiler.go
+++ internal/compiler/compiler.go
@@ -1,16 +1,15 @@
 package compiler
 
 import (
+	"github.com/yasufadhili/jawt/internal/ast"
 	"github.com/yasufadhili/jawt/internal/core"
 	"github.com/yasufadhili/jawt/internal/diagnostic"
 )
 
-// Compiler compiles JML files
 type Compiler struct {
 	ctx *core.JawtContext
 }
 
-// NewCompiler creates a new compiler.
 func NewCompiler(ctx *core.JawtContext) *Compiler {
 	return &Compiler{
 		ctx: ctx,
