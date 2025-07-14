b3c371aac60fbd8d781816dd1543b8ba69bd843a
Yasu Fadhili <yasufad@proton.me>
2025-07-14 14:46:57 +0100

    add build system setup

--- internal/build/builder.go
+++ internal/build/builder.go
@@ -1,6 +1,11 @@
 package build
 
-import "time"
+import (
+	"github.com/fsnotify/fsnotify"
+	"github.com/yasufadhili/jawt/internal/core"
+	"sync"
+	"time"
+)
 
 // DocumentType represents the type of document (page or component)
 type DocumentType int
@@ -33,4 +38,34 @@ type ComponentInfo struct {
 type PageInfo struct {
 	DocumentInfo
 	Route string
+}
+
+type BuildSystem struct {
+	ctx   *core.JawtContext
+	mu    sync.RWMutex
+	docs  map[string]*DocumentInfo
+	pages map[string]*PageInfo
+	comps map[string]*ComponentInfo
+	// compiler *compiler.Compiler
+	watcher  FileWatcher
+	depGraph DependencyGraph
+}
+
+// FileWatcher is an interface for watching files for changes
+type FileWatcher interface {
+	OnEvent(handler func(fsnotify.Event))
+	Start() error
+	Stop() error
+}
+
+// NewBuildSystem creates a new BuildSystem
+func NewBuildSystem(ctx *core.JawtContext, watcher FileWatcher) *BuildSystem {
+	return &BuildSystem{
+		ctx:      ctx,
+		docs:     make(map[string]*DocumentInfo),
+		pages:    make(map[string]*PageInfo),
+		comps:    make(map[string]*ComponentInfo),
+		watcher:  watcher,
+		depGraph: NewDependencyGraph(),
+	}
 }
